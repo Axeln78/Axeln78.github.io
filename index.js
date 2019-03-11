89503fe9fa6f9ffd99e060fd22d0048750961292
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2019-03-11 09:32:11 +0100

    File not needed

--- index.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs')
-var gexf = require('ngraph.gexf')
-var graph = gexf.load(fs.readFileSync('myfile.gexf', 'utf8'))
-// graph is now normal grpah and can be used by ngraph modules
