8f85833a49d12b6d63644f5739a25c3ac003c7dc
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2019-04-20 11:59:59 +0200

    Website re-looking: all the menues are now collapsible to give more space to the visualisation

--- index.js
+++ index.js
@@ -425,3 +425,18 @@ function PlotI(nodes) {
     console.log(document.getElementById("range").max);
   });
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
